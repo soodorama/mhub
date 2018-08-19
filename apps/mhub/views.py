ca64bb2ab9a6bd482e6f6b4b4dd6eb36fe42ea92
Neil Sood <neilsood@Neils-MBP.hsd1.ca.comcast.net>
2018-08-19 01:33:55 -0700

    hello world

--- apps/mhub/views.py
+++ apps/mhub/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
-    render(request, "mhub/index.html")
+    return render(request, "mhub/index.html")
