30cbfb684b844950437582f1674a0d083cbfe619
ashwinchalaka <ashwin.chalaka@gmail.com>
2018-08-22 09:50:06 -0700

    branching changes trial1

--- apps/mhub/urls.py
+++ apps/mhub/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='home'),
-    url(r'^bio$', views.bio, name='bio'),
-    url(r'^search$', views.search, name='search'),
+    url(r'^$', views.index, name='go_home'),
+    url(r'^bio$', views.bio, name='go_bio'),
+    url(r'^ping$', views.ping, name='process_pings'),
+    url(r'^pong$', views.pong, name='go_pongs'),
+    url(r'^logReg$', views.logReg, name='go_logReg'),
+    url(r'^process/login$', views.processLogin, name='process_login'),
+    url(r'^process/registration$', views.processRegistration, name='process_regis'),
 ]
--- apps/mhub/views.py
+++ apps/mhub/views.py
@@ -3,9 +3,21 @@ from django.shortcuts import render, redirect
 def index(request):
     return render(request, "mhub/index.html")
 
+def logReg(request):
+    return render(request, "mhub/logReg.html")
+
+def processLogin(request):
+    return redirect(reverse("go_home"))
+
+def processRegistration(request):
+    return redirect(reverse("go_logReg"))
+
 def bio(request):
     return render(request, "mhub/bio.html")
 
-def search(request):
+def ping(request):
     print(request.POST['search'])
-    return redirect('home')
+    return redirect('go_home')
+
+def pong(request):
+    return render(request, "mhub/morePongs.html")
