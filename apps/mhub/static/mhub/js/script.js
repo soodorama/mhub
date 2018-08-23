22f471c092f28303c5e2515dbdca2075524f0ea4
Neil Sood <neilsood@Neils-MacBook-Pro.local>
2018-08-23 15:05:29 -0700

    video player embedded

--- apps/mhub/static/mhub/js/script.js
+++ apps/mhub/static/mhub/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(".search_results").hide();
-    $('.loading_pingpong').hide();
+    // $('.loading_pingpong').hide();
 
     $("#submit_search").submit(function(e) {
         e.preventDefault();
@@ -36,50 +36,49 @@ $(document).ready(function() {
             }
         }, 'json')
         
-        // This code loads the IFrame Player API code asynchronously.
-        var tag = document.createElement('script');
-        
-        tag.src = "https://www.youtube.com/iframe_api";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        
-        // This function creates an <iframe> (and YouTube player)
-        // after the API code downloads.
-        var player;
-        function onYouTubeIframeAPIReady() {
-            player = new YT.Player('player', {
-                height: '390',
-                width: '640',
-                videoId: 'M7lc1UVf-VE',
-                events: {
-                    'onReady': onPlayerReady,
-                    'onStateChange': onPlayerStateChange
-                }
-            });
-        }
-        
-        // The API will call this function when the video player is ready.
-        function onPlayerReady(event) {
-            event.target.playVideo();
-        }
     })
     
+    // This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
     
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    
+    // This function creates an <iframe> (and YouTube player)
+    // after the API code downloads.
+    var player;
+    function onYouTubeIframeAPIReady() {
+        console.log("hi")
+        player = new YT.Player('player', {
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+    
+    // The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
+    
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    //    the player should play for six seconds and then stop.
+    var done = false;
+    function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+        }
+    }
+    function stopVideo() {
+        player.stopVideo();
+    }
+
 })
 
-        // // 5. The API calls this function when the player's state changes.
-        // //    The function indicates that when playing a video (state=1),
-        // //    the player should play for six seconds and then stop.
-        // var done = false;
-        // function onPlayerStateChange(event) {
-        //     if (event.data == YT.PlayerState.PLAYING && !done) {
-        //     setTimeout(stopVideo, 6000);
-        //     done = true;
-        //     }
-        // }
-        // function stopVideo() {
-        //     player.stopVideo();
-        // }
 
 
 
